ebb9489091d8cc230f1d5c7ebecd0115778eea33
Marcus Creese <marcuscreese94@gmail.com>
2022-03-01 10:10:57 -0600

    deleted unused methods

--- src/main/java/com/revature/carapp/CarController.java
+++ src/main/java/com/revature/carapp/CarController.java
@@ -10,13 +10,10 @@ import java.io.IOException;
 public class CarController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("carModel");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int mpg = Integer.parseInt(req.getParameter("mpg"));
-        String carModel = req.getParameter("carModel");
     }
 }
 
