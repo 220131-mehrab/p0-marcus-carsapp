ef6cefed694d1c2494c7751656ec99adf83cc06b
Marcus Creese <marcuscreese94@gmail.com>
2022-03-02 09:21:41 -0600

    deleted unused methods

--- src/main/java/com/revature/carapp/App.java
+++ src/main/java/com/revature/carapp/App.java
@@ -1,11 +1,5 @@
 package com.revature.carapp;
 
-
-import org.apache.catalina.LifecycleException;
-
-import java.util.List;
-
-
 public class App {
     public static void main(String[] args) {
         CarRepository carObject = new CarRepository();
